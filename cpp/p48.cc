131a9aa9222e2588ec285e8e11ceec04cccc4dbb
Tyler Brabham <tylerbrabham@hotmail.com>
2014-10-28 22:15:59 -0700

    readme

---
+++ cpp/p48.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <math.h>
+
+int[] get_zero_vec() {
+
+}
+
+int[] pad() {
+
+}
+
+int[] int2vec() {
+
+}
+
+int vec2int() {
+
+}
+
+int[] add() {
+
+}
+
+int[] multiply() {
+
+}
+
+int[] self_power() {
+
+}
+
+int main() {
+	return 0;
+}
