31803f3a2217e01f225a2d14f0afaef9e7043ead
Tyler Brabham <tylerbrabham@hotmail.com>
2014-11-02 10:55:03 -0800

    p67 working, but ugly

---
+++ haskell/p67/p67.hs
@@ -0,0 +1,28 @@
+import Data.Char
+import Data.List
+
+convertStr x = read x :: Int
+
+toIntList xs = map convertStr xs
+
+splitContents contents = map words (lines contents)
+
+updateMaxPathRow _ [] c = c
+updateMaxPathRow [] _ c = c
+updateMaxPathRow a b c = updateMaxPathRow (tail a) (tail b) (c ++ [d])
+  where
+    d = maximum (take 2 a) + head b
+
+p67 :: [[Int]] -> [Int] -> Int
+p67 [] maxPathRow = maximum maxPathRow
+p67 (x : xs) maxPathRow = p67 xs (updateMaxPathRow maxPathRow ys newMaxRow)
+  where
+    newMaxRow = [head maxPathRow + y]
+    ys = tail x
+    y = head x
+
+main = do  
+  contents <- readFile "p67.txt"
+  let triangle = map toIntList (splitContents contents)
+
+  print $ p67 triangle [0]
