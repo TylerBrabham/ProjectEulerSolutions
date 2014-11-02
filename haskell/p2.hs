0bf66d0f8eddebe348f8df17360b3e661a2d74d6
Tyler Brabham <tylerbrabham@hotmail.com>
2014-11-01 22:35:40 -0700

    p2

--- haskell/p2.hs
+++ haskell/p2.hs
@@ -1,4 +1,10 @@
-p2 = 5
+p2 a b acc
+  | b > 4000000 = acc
+  | otherwise = p2 b (a + b) (acc + c)
+    where 
+      c = if b `mod` 2 == 0 
+          then b
+          else 0
 
 main = do
-  print p2
+  print (p2 0 1 0)
