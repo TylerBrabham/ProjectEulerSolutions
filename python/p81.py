40ba54c743ee239a8479e3082b204a5cec072684
Tyler Brabham <tylerbrabham@hotmail.com>
2014-11-03 08:44:24 -0800

    spaces

--- python/p81.py
+++ python/p81.py
@@ -1,58 +1,65 @@
-def read_matrix():
-	matrix = []
-	with open('p81.txt') as f:
-		for line in f:
-			split_line = line.strip().split(',')
-			int_row = [int(x) for x in split_line]
-			matrix.append(int_row)
+"""
+This file contains two solutions to problem 81. The first is iterative and fills
+out a table diagonally in a standard dp way. The second is the natural recursive 
+solution. The second solution is too inefficient to work on the 80 by 80 input
+matrix.
+"""
 
-	return matrix
+def read_matrix():
+  matrix = []
+  with open('p81.txt') as f:
+    for line in f:
+      split_line = line.strip().split(',')
+      int_row = [int(x) for x in split_line]
+      matrix.append(int_row)
+
+  return matrix
 
 def p81():
-	matrix = read_matrix()
+  matrix = read_matrix()
 
-	# A_ij is the best solution for the first i rows and first j columns
+  # A_ij is the best solution for the first i rows and first j columns
 
-	# Add extra row and column of inf's so that we don't have to have several if
-	# statements later.
-	A = [[float('inf') for i in range(81)]]
-	for i in range(80):
-		A.append([float('inf')] + [0 for j in range(80)])
+  # Add extra row and column of inf's so that we don't have to have several if
+  # statements later.
+  A = [[float('inf') for i in range(81)]]
+  for i in range(80):
+    A.append([float('inf')] + [0 for j in range(80)])
 
-	A[1][1] = matrix[0][0]
+  A[1][1] = matrix[0][0]
 
-	# A_ij depends on A_i-1_j and A_i_j-1. So we should fill out the values in a 
-	# diagonal fashion.
-	start_column = 1
-	start_row = 1
+  # A_ij depends on A_i-1_j and A_i_j-1. So we should fill out the values in a 
+  # diagonal fashion.
+  start_column = 1
+  start_row = 1
 
-	while start_column <= 80 and start_row <= 80:
-		if start_column < 80:
-			start_column += 1
-			start_row = 1
-		else:
-			start_row += 1
+  while start_column <= 80 and start_row <= 80:
+    if start_column < 80:
+      start_column += 1
+      start_row = 1
+    else:
+      start_row += 1
 
-		i = start_row
-		j = start_column
+    i = start_row
+    j = start_column
 
-		while j >= 1 and i <= 80:
-			A[i][j] = min(A[i - 1][j], A[i][j - 1]) + matrix[i - 1][j - 1]
+    while j >= 1 and i <= 80:
+      A[i][j] = min(A[i - 1][j], A[i][j - 1]) + matrix[i - 1][j - 1]
 
-			j -= 1
-			i += 1
+      j -= 1
+      i += 1
 
-	return A[80][80]
+  return A[80][80]
 
 def p81_recursive(matrix, i, j):
-	if i == 0 and j == 0:
-		return matrix[i][j]
-	elif i == 0:
-		return p81_recursive(matrix, i, j - 1) + matrix[i][j]
-	elif j == 0:
-		return p81_recursive(matrix, i - 1, j) + matrix[i][j]
-	else:
-		return min(p81_recursive(matrix, i - 1, j), p81_recursive(matrix, i, j - 1)) + matrix[i][j]
+  if i == 0 and j == 0:
+    return matrix[i][j]
+  elif i == 0:
+    return p81_recursive(matrix, i, j - 1) + matrix[i][j]
+  elif j == 0:
+    return p81_recursive(matrix, i - 1, j) + matrix[i][j]
+  else:
+    return min(p81_recursive(matrix, i - 1, j), p81_recursive(matrix, i, j - 1)) + matrix[i][j]
 
 if __name__ == "__main__":
-	print p81_recursive(read_matrix(), 4, 79)
+  print p81_recursive(read_matrix(), 4, 79)
