4e3c366490fd65cd6bc9dcdbef5418f6dd22d19d
Tyler Brabham <tylerbrabham@hotmail.com>
2014-11-29 00:21:09 -0800

    haskell p37

--- haskell/p37.hs
+++ haskell/p37.hs
@@ -2,5 +2,21 @@ import Util.Cast
 import Util.Sieve
 import qualified Data.Map as Map
 
+leftStrips [] = []
+leftStrips [x] = []
+leftStrips (x : xs) = xs : (leftStrips xs)
+
+rightStrips xs = map reverse (leftStrips $ reverse xs)
+
 strips :: Int -> [Int]
-strips x = undefined
+strips x = y ++ (map digitsToInt $ leftStrips d) ++ (map digitsToInt $ rightStrips d)
+  where d = toDigits x
+        y = if length d > 1 then [x] else []
+
+p37 = sum xs
+  where (primes, pmap) = allPrimesWithMap 1000000
+        xs = filter (\x -> (all (\k -> Map.member k pmap) (strips x))) largePrimes
+        largePrimes = drop 4 primes
+
+main = do
+  print $ p37
